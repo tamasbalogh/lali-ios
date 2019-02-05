cfd9570ae8e20d5888635a811f13de7ad2dc242c
tamasbalogh <baloght@gain.uni-sopron.hu>
2019-02-05 18:32:44 +0100

    Initial Commit

---
+++ lali/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lali
+//
+//  Created by Balogh Tamas on 2019. 02. 05..
+//  Copyright © 2019. Balogh Tamas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
